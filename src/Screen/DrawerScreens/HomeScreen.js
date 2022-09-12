e43db388bafe24a7114c9242a9c65026865c4033
Joseph J <joseph.j@infospica.com>
2022-09-12 19:07:14 +0530

    update

--- src/Screen/DrawerScreens/HomeScreen.js
+++ src/Screen/DrawerScreens/HomeScreen.js
@@ -1,28 +1,47 @@
 
-import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import React ,{useState, useEffect}from 'react';
+import {View, Text, SafeAreaView,TouchableOpacity, StyleSheet,} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
  
 const HomeScreen = () => {
+  const [userdata, setUserData] = useState('');
+  const [dflag, setdflag] = useState(false);
+  useEffect(async() => {
+      const userData = JSON.parse(await AsyncStorage.getItem('user_id')).AsyncStorage.then((value) =>
+      setUserData(value));
+    }, []);
+   
+    const getdetail=()=>{
+   setdflag(!dflag);
+    }
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1, padding: 16}}>
-        <View
+      <TouchableOpacity
+             style={styles.buttonStyle}
+             activeOpacity={0.5}
+              onPress={getdetail}>
+              <Text >Get user details</Text>
+            </TouchableOpacity>
+            {dflag=='true' &&
+       <View
           style={{
             flex: 1,
             alignItems: 'center',
             justifyContent: 'center',
           }}>
+         
           <Text
             style={{
               fontSize: 20,
               textAlign: 'center',
               marginBottom: 16,
             }}>
-            Example of Splash, Login and Sign Up in React Native
-            {'\n\n'}
-            This is the Home Screen
+            {{userdata}}
+          
           </Text>
-        </View>
+   
+        </View>}
         <Text
           style={{
             fontSize: 18,
@@ -43,6 +62,23 @@ const HomeScreen = () => {
     </SafeAreaView>
   );
 };
- 
+
 export default HomeScreen;
 
+const styles = StyleSheet.create({
+ 
+  buttonStyle: {
+    backgroundColor: 'pink',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#7DE24E',
+    height: 40,
+    alignItems: 'center',
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 25,
+  },
+
+});
